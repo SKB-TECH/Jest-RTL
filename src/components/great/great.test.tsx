cedcd86d4ad0dbc39d1a8d779a2c66eee90a7c3f
benjamin <shakokinyamba201@gmail.com>
2024-02-02 05:09:15 -0500

    feat: tests grouping

--- src/components/great/great.test.tsx
+++ src/components/great/great.test.tsx
@@ -1,23 +1,24 @@
-import {render,screen} from "@testing-library/react";
+  import {render,screen} from "@testing-library/react";
 import {Great} from "./great";
 
 
 describe("Great",()=>{
-    test("hello all",()=>{
+    it("hello all",()=>{
         //create Virtuel Dom of Component
-        render(<Great name={"Ben"}/>)
+        render(<Great name={"Ben"} />)
         //verify in the screen element
         const textHello=screen.getByText(/hello/i)
         //where we will search it (in the document)
         expect(textHello).toBeInTheDocument()
     })
-
-    test("hello ben",()=>{
-        //create Virtuel Dom of Component
-        render(<Great name={"Ben"}/>)
-        //verify in the screen element
-        const textHello=screen.getByText(/hello/i)
-        //where we will search it (in the document)
-        expect(textHello).toBeInTheDocument()
+    describe("Geat Nest",()=>{
+        it("hello ben",()=>{
+            //create Virtuel Dom of Component
+            render(<Great name={"Ben"}/>)
+            //verify in the screen element
+            const textHello=screen.getByText(/hello/i)
+            //where we will search it (in the document)
+            expect(textHello).toBeInTheDocument()
+        })
     })
 })
